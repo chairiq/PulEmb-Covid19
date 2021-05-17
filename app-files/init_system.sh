96bba8a34f491e86ffa22b12f85d8efa600fc027
Chairi Kiourt <35818737+chairiq@users.noreply.github.com>
2021-05-17 11:54:31 +0300

    Update init_system.sh

--- app-files/init_system.sh
+++ app-files/init_system.sh
@@ -15,7 +15,7 @@ make
 
 mv /content/PulEmb-Covid19/support-files/yolov4-ctscans.cfg cfg/yolov4-ctscans.cfg
 mv /content/PulEmb-Covid19/support-files/ctscans.names ./data
-mv /content/PulEmb-Covid19/support-files//ctscans.data  ./data
-mv /content/PulEmb-Covid19/support-files//ctscans.data  ./data
-mv /content/PulEmb-Covid19/support-files//yolov4-ctscans_best.weights ./data
-mv /content/PulEmb-Covid19/support-files//classification_model  ./classification_model
+mv /content/PulEmb-Covid19/support-files/ctscans.data  ./data
+mv /content/PulEmb-Covid19/support-files/ctscans.data  ./data
+mv /content/PulEmb-Covid19/support-files/yolov4-ctscans_best.weights ./data
+mv /content/PulEmb-Covid19/support-files/classification_model  ./classification_model
